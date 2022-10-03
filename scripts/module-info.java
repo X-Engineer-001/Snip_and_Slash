9d78d4756e31290af46963baed881696a3f370f9
Novice Engineer <fabin1790@gmail.com>
2022-10-03 18:30:47 +0800

    Update module-info.java

--- scripts/module-info.java
+++ scripts/module-info.java
@@ -1,8 +1,8 @@
 module snip_and_slash {
 	requires java.desktop;
 	requires transitive javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.fxml;
 	opens server to javafx.fxml;
 	opens client to javafx.fxml;
 	exports server;
